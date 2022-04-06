12cf53fcae7e1be403ca000db0769cd6496bc3d7
yazawa <nicoberndt234@gmail.com>
2022-04-06 22:57:08 +0200

    buttons haben jetzt animationen

--- docs/js_code/code.js
+++ docs/js_code/code.js
@@ -1,1 +1,11 @@
+window.onload = function(){
+    let canvas = document.getElementById("mainCanvas");
+    let ctx = canvas.getContext("2d", {antialias: true});
 
+    ctx.moveTo(50, 100);
+    ctx.lineTo(400, 200);
+    ctx.stroke();
+
+    ctx.rect(400, 200, 100, 100);
+    ctx.stroke();
+}
