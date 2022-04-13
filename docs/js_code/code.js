527515f831bc31c3d7d1cf62d1f85c2e9721d8d0
yazawa <nicoberndt234@gmail.com>
2022-04-13 18:28:21 +0200

    working on language selector

--- docs/js_code/code.js
+++ docs/js_code/code.js
@@ -1,11 +1,21 @@
 window.onload = function(){
     let canvas = document.getElementById("mainCanvas");
     let ctx = canvas.getContext("2d", {antialias: true});
+    var select = document.getElementById("selectLang");
+    var de = "./index.html";
+    var de = "./englishIndex.html";
 
+    ctx.beginPath()
     ctx.moveTo(50, 100);
     ctx.lineTo(400, 200);
     ctx.stroke();
 
+    ctx.beginPath()
     ctx.rect(400, 200, 100, 100);
     ctx.stroke();
+
+
+    select.onchange = function() {
+        location.de = this.german;
+    }
 }
