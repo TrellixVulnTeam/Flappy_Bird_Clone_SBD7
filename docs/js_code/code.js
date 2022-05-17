573b383df967fa65cd72d8ff7506c7904ad7c3cc
yazawa <nicoberndt234@gmail.com>
2022-05-17 09:19:24 +0200

    installed sqlite3 and sqlite

--- docs/js_code/code.js
+++ docs/js_code/code.js
@@ -54,9 +54,6 @@ let lastFrameTime = 0;
 let fps;
 
 // Database
-const Datastore = require("nedb");
-const database = new Datastore("database.db");
-database.loadDatabase();
 
 
 // TODO: !!!  i need to make every size relative to the screen ... also the speed of everything !!!
